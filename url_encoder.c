12dad2f8414963bf637b68fd77205488559dbaf3
Madhav Gautam <gautam.madhav63@gmail.com>
2026-07-19 12:09:05 +0530

    Implement in place URL encoding by replacing spaces with %20

---
+++ url_encoder.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void url_encode(char *str, int true_length) {
+
+    int spaces = 0, i, index;
+
+    for (i = 0; i < true_length; i++) {
+        if (str[i] == ' ')
+            spaces++;
+    }
+
+    index = true_length + spaces * 2;
+    str[index] = '\0';
+
+    for (i = true_length - 1; i >= 0; i--) {
+        if (str[i] == ' ') {
+            str[index - 1] = '0';
+            str[index - 2] = '2';
+            str[index - 3] = '%';
+            index -= 3;
+        } else {
+            str[index - 1] = str[i];
+            index--;
+        }
+    }
+}
+
+int main() {
+
+    char str[30] = "Hello World";
+
+    url_encode(str, 11);
+    printf("%s\n", str);
+
+    return 0;
+}
