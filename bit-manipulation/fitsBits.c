1e6ca7401552cd10b3ff38e9f5e3eb463b75e50a
Madhav Gautam <gautam.madhav63@gmail.com>
2026-08-02 11:02:40 +0530

    Implement fitsBits using bitwise operations

---
+++ bit-manipulation/fitsBits.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int fitsBits(int x, int n) {
+
+    int shift = 32 + (~n + 1);
+    return !(((x << shift) >> shift) ^ x);
+}
+
+int main() {
+    
+    printf("fitsBits(5, 3) = %d\n", fitsBits(5, 3));
+    printf("fitsBits(-4, 3) = %d\n", fitsBits(-4, 3));
+
+    return 0;
+}
